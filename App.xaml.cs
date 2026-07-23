009ed2b2a984b8bd2080e0ebfdb88444444d8ca7
LordChunga <alecazon14@gmail.com>
2026-07-23 19:32:08 -0300

    chore: Add global exception handler for diagnostics

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,6 +13,19 @@ public partial class App : Application
     public App()
     {
         SqlMapper.AddTypeHandler(new DecimalTypeHandler());
+        this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+    }
+
+    private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+        System.IO.File.AppendAllText("log.txt", "\n[UI CRASH] " + e.Exception.ToString() + "\n");
+        e.Handled = true; // prevent exit to see if we can continue, but it might still crash if critical
+    }
+
+    private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        System.IO.File.AppendAllText("log.txt", "\n[BG CRASH] " + e.ExceptionObject.ToString() + "\n");
     }
 
     private class DecimalTypeHandler : SqlMapper.TypeHandler<decimal>
